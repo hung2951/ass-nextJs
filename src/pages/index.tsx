faaa186413e7df9ba8a683d3cfffada76b9d83c4
van hung <hungtv290501@gmail.com>
2022-07-20 10:57:00 +0700

    first commit

---
+++ src/pages/index.tsx
@@ -0,0 +1,12 @@
+import type { NextPage } from 'next'
+
+
+const Home: NextPage = () => {
+  return (
+    <h1 className="bg-red-800 font-bold underline">
+      Hello world!
+  </h1>
+  )
+}
+
+export default Home
