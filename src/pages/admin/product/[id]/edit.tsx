f4ad144d811d515e149d6d2bdf205278a3ed89dc
van hung <hungtv290501@gmail.com>
2022-07-26 19:17:27 +0700

    feat: create, edit, remove product

---
+++ src/pages/admin/product/[id]/edit.tsx
@@ -0,0 +1,77 @@
+import LayoutAdmin from '@/components/layouts/LayoutAdmin'
+import { useProduct } from '@/hooks/product'
+import { useRouter } from 'next/router'
+import React from 'react'
+import { SubmitHandler, useForm } from 'react-hook-form'
+import useSWR from 'swr'
+
+type Props = {}
+type Inputs = {
+    name:string,
+    price:number,
+    discount:number,
+    quantity:number,
+    img:string,
+    category:string,
+    desc:string
+}
+const ProductEdit = (props: Props) => {
+    const router = useRouter();
+    const { id } = router.query;
+    const {updateItem} =useProduct();
+    const {register,handleSubmit,formState:{errors}} = useForm<Inputs>()
+    const {data:product,error} = useSWR(id ? `/products/${id}` : null);
+    if (!product) return <div>Loading...</div>;
+    if (error) return <div>Loading to failed</div>;
+    console.log(product);
+    
+    const onSubmit:SubmitHandler<Inputs> = data=>{
+        updateItem(id,data)
+    }
+  return (
+    <div>
+        <div className="container mt-2">
+            <div className='text-center'>Product Edit</div>
+            <form onSubmit={handleSubmit(onSubmit)}>
+                <div className=" col-span-6 sm:col-span-4 py-[30px] ">
+                    <label className="block text-sm font-medium text-gray-700 ">Name</label>
+                    <input type="text "{...register('name')} defaultValue={product.name} className="form-control " id="" placeholder=""/>
+                </div>
+
+                <div className="col-span-6 sm:col-span-4 pb-[30px] ">
+                    <label className="block text-sm font-medium text-gray-700 ">Price</label>
+                    <input type="number "{...register('price')} defaultValue={product.price} className="form-control"/>
+                </div>
+                <div className="col-span-6 sm:col-span-4 pb-[30px] ">
+                    <label className="block text-sm font-medium text-gray-700 ">Discount</label>
+                    <input type="number " {...register('discount')}  defaultValue={product.discount}  className="form-control"/>
+                </div>
+                <div className="col-span-6 sm:col-span-4 pb-[30px] ">
+                    <label className="block text-sm font-medium text-gray-700 ">Quantity</label>
+                    <input type="number "{...register('quantity')} defaultValue={product.quantity} className="form-control"/>
+                </div>
+                <div className="col-span-6 sm:col-span-4 pb-[30px] ">
+                    <label className="block text-sm font-medium text-gray-700 ">Image</label>
+                    <input type="text"{...register('img')} defaultValue={product.img} className="form-control " />
+                </div>
+                <div className="col-span-6 sm:col-span-4 pb-[30px] ">
+                    <label className="block text-sm font-medium text-gray-700 ">Category</label>
+                    <select className="form-select mb-3" {...register('category')} aria-label="Default select example">
+                        <option  value={"62df9cfe8646f2e5450f836b"} >Danh mục 1</option>
+                        <option  value={"62df9d088646f2e5450f836d"} >Danh mục 2</option>
+                </select>
+                </div>
+                <div className="col-span-6 sm:col-span-4 ">
+                    <label className="block text-sm font-medium text-gray-700 ">Details</label>
+                    <div className="mb-6 ">
+                        <textarea id="details " {...register('desc')} rows={3} className="shadow-sm focus:ring-indigo-500 focus:border-indigo-500 mt-1 block w-full sm:text-sm border border-gray-300 rounded-md p-[20px] ">{product.desc}</textarea>
+                    </div>
+                </div>
+                <button className="btn btn-success my-[30px] px-[30px] bg-[#198754] " >Submit</button>
+            </form>
+        </div>
+    </div>
+  )
+}
+ProductEdit.Layout = LayoutAdmin
+export default ProductEdit
--- src/pages/admin/product/index.tsx
+++ src/pages/admin/product/index.tsx
@@ -1,11 +1,50 @@
 import LayoutAdmin from '@/components/layouts/LayoutAdmin'
+import { useProduct } from '@/hooks/product'
+import Link from 'next/link'
 import React from 'react'
 
 type Props = {}
 
 const ProductList = (props: Props) => {
+  const {data:product,error,remove} = useProduct()
+  if(!product) return <div>Loading...</div>
+  if(error) return <div>Loading to failed</div>
   return (
-    <div>Product</div>
+    <div>
+       <div className="container mt-5">
+        <div className='text-center'>Product Management</div>
+        <Link href={`/admin/product/add`}><button className='text-blue-600 text-sm underline'>Create product</button></Link>
+          <table className="table table-borderless table-responsive card-1 p-4 text-base text-center">
+              <thead>
+                  <tr className="border-bottom">
+                      <th> <span className="ml-2 ">STT</span> </th>
+                      <th> <span className="ml-2">Name</span> </th>
+                      <th> <span className="ml-2">Image</span> </th>
+                      <th> <span className="ml-2">Price</span> </th>
+                      <th> <span className="ml-2">Status</span> </th>
+                      <th colSpan={2} className=""> <span className="pl-[50px]">Action</span> </th>
+                  </tr>
+              </thead>
+              <tbody>
+                {product.map((item:any,index:number)=>(
+                  <tr className="border-bottom" key={item._id}>
+                      <td className="">{index+1}</td>
+                      <td>{item.name}</td>
+                      <td><img src={item.img} alt="" width="100px"/></td>
+                      <td>{item.price}</td>
+                      <td>{item.status==true?"Activated":"Disable"}</td>
+                      <td colSpan={2}>
+                        <Link href={`/admin/product/${item._id}/edit`}>
+                          <button className="btn btn-success" >Edit</button>
+                        </Link>
+                        <button className="btn btn-danger" onClick={()=>{remove(item._id)}}>Remove</button>
+                      </td>
+                  </tr>
+                ))}
+              </tbody>
+          </table>
+      </div>
+    </div>
   )
 }
 ProductList.Layout = LayoutAdmin
