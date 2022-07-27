52e491a23d7046cad68b9a27482ba0993f90bf1f
Tungduong2001 <tungduong.kaka64@gmail.com>
2022-07-27 10:28:32 +0700

    ASS-25 <list-users>

--- src/api/instance.tsx
+++ src/api/instance.tsx
@@ -1,7 +1,11 @@
 import axios from "axios";
 
 const instance = axios.create({
-  baseURL:'http://localhost:8000'
+  baseURL:'http://localhost:8000/api'
 })
-
+instance.interceptors.response.use(function (response) {
+  return response.data;
+}, function (error) {
+  return Promise.reject(error);
+});
 export default instance
