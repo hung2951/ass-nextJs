32132987e287e1cec9371a42c4a6e5dd6c572cc8
van hung <hungtv290501@gmail.com>
2022-07-28 22:56:41 +0700

    update

--- src/components/client/products.tsx
+++ src/components/client/products.tsx
@@ -17,18 +17,17 @@ const Productsclient = (props: Props) => {
                 {data.map((item: any) => (
                     <form action="" key={item._id}>
                         <div className=" khoiy border-solid border-2 border-[#f3f3f3] rounded-lg " >
-                            <div className=" w-[66%]  m-auto ">
-                                <Link href={"./products/123456"}>
-                                    <div>
+                            <div className=" w-[66%]  m-auto">
+                                <Link href={`/products/${item._id}`}>
+                                    <div className='pt-3'>
                                         {/* <img src="https://image.made-in-china.com/202f0j00KZUiGpwMLhos/M9-Plus-7-0inch-12g-512g-24MP-Cheap-Smart-Phones-4G-Mobile-Android-Cell-Phones-Lowest-Price-Mobail-Phone.webp" height="100px" /> */}
                                         <img src={item.img} height="100px" />
-
                                     </div>
                                 </Link>
                             </div>
-                            <div className="py-[10px] px-[10px] leading-8">
-                                <Link href={"./products/123456"} className="no-underline">
-                                    <button className="no-underline text-blue-600 visited:text-purple-600 hover:text-red-500">{item.name}</button>
+                            <div className="py-[10px] px-[10px] leading-8 text-center">
+                                <Link href={"./products/123456"} className="no-underline ">
+                                    <button className="no-underline mx-auto text-blue-600 visited:text-purple-600 hover:text-red-500">{item.name}</button>
                                 </Link>
                                 <p className="text-[15px] text-[red]">{item.price}</p>
                                 <p>130,000,000 lượt xem</p>
@@ -38,7 +37,6 @@ const Productsclient = (props: Props) => {
                             </div>
                         </div>
                     </form>
-
                 ))}
 
 
