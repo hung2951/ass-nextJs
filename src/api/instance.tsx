e9db891c0cefae449f1936d14e9aabc4fd1bb139
Văn Hùng <94148492+hung2951@users.noreply.github.com>
2022-11-28 17:32:02 +0700

    update

--- src/api/instance.tsx
+++ src/api/instance.tsx
@@ -1,7 +1,7 @@
 import axios from "axios";
 
 const instance = axios.create({
-  baseURL: 'http://localhost:8000/api'
+  baseURL: 'https://node-next-js.vercel.app/api'
 })
 instance.interceptors.response.use(function (response) {
   return response.data;
